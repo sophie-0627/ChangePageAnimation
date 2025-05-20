5dc4ba956b7aa4f6e2d3df70d078f37e9b0462fb
sofia <sofia0627sofia@gmail.com>
2025-05-21 00:07:20 +0800

    dd

---
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.simulateHotReload
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.test.ui.theme.TestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Test()
+        }
+    }
+}
+
+@Composable
+fun Test() {
+    val pageState = rememberPagerState(pageCount = { 2 })
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(top = 30.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        HorizontalPager(state = pageState) { page ->
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Blue)
+                    .padding(top = 200.dp)
+            ) {
+                Text(page.toString())
+            }
+        }
+
+        Row (
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Center
+        ){
+            repeat(pageState.pageCount) { index ->
+                Box(
+                    modifier = Modifier
+                        .animateContentSize()
+                        .clip(RoundedCornerShape(3.dp))
+                        .height(10.dp)
+                        .background(if(pageState.currentPage == index) Color.DarkGray else Color.LightGray)
+                        .width(if (pageState.currentPage == index) 100.dp else 10.dp)
+                )
+            }
+        }
+    }
+}
